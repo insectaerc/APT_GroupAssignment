e37f6eb809512ae82b5be7a4eb42a9f3a4d6efed
harrrywon <tranminhquan2001bd@gmail.com>
2022-05-18 08:19:07 +0700

    .

---
+++ house.cpp
@@ -0,0 +1,13 @@
+#include "House.h"
+#include <iostream>
+
+House::House(){}
+House::House(std::string owner, std::string location, std::string description){
+    this->owner = owner;
+    this->location = location;
+    this->description = description;
+}
+
+void House::showInfo(){
+    std::cout << "Owner: " << this->owner << ", location: " << this->location << ", description: " << this->description << "\n";
+}
