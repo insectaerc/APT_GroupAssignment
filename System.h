33b9c1f414a453550b10dc74bbcf28e7c121786d
insectaerc <agilastic@gmail.com>
2022-05-18 21:24:30 +0700

    Add new file for System Class
    _Move functions in main.cpp to to System class

---
+++ System.h
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+#include "Member.h"
+#include "House.h"
+
+//DEFINE
+#define MAXPASSLENGTH 20
+
+class System{
+    public:
+        //Attributes
+        bool isLoggedInVar;
+        Member *loggedInMember;
+        std::vector<Member*> members;
+        std::vector<House*> houses;
+        std::vector<std::string> locations;
+
+        //Constructor
+        System();
+
+        //Functions
+        void loadData();
+        void saveData();
+        void mainMenu();
+        void guestMenu();
+        void memberMenu();
+        void loginInput(std::string *u, std::string *p);
+        bool isLoggedIn(std::string username, std::string password);
+        std::string toLowercase(std::string str);
+        void showHousesGuest();
+        void showHousesMember();
+};
